a227e46430aac648d6dafe40e075094ba54b2841
gqgunhed <gunther@5stengl.de>
2015-02-02 16:35:23 +0100

    added showpacket()

--- udp_forwarder.py
+++ udp_forwarder.py
@@ -4,9 +4,18 @@
 from scapy.all import *
 
 
-PORTS = [ 53, 8888 ]            # list of ports
+PORTS = [ 8888 ]                # list of ports
 OLDDEST = "10.0.2.255"          # the original broadcast address
 NEWDEST = "127.0.0.10"          # the new destination of the packet
+
+
+def showpacket(pkt, message=None):
+    """shows packet via scapy's show() function"""
+    if message:
+        print 15 * "-",
+        print message,
+        print print 15 * "-"
+    pkt.show()
 
 
 def exchange_destination(pkt):
@@ -14,7 +23,7 @@ def exchange_destination(pkt):
     leave all other fields untouched
     """
     pkt[IP].dst = NEWDEST
-    pkt.show()                  # shows the modified packet
+    showpacket(pkt, "modified") # shows the modified packet
 
 
 def resend_packet(pkt):
@@ -25,7 +34,7 @@ def resend_packet(pkt):
 def udp_forward(pkt):
     """if packet matches, modify it and re-send to new location"""
     if UDP in pkt and pkt[IP].dst==OLDDEST and pkt[UDP].dport in PORTS:
-        #pkt.show()              # shows the original/unaltered packet
+        showpacket(pkt, "original")
         exchange_destination(pkt)
         resend_packet(pkt)
 
